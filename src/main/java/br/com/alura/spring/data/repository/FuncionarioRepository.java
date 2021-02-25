447c16b14e1b8bbe2836ebdb7e1773856e4a1f85
Luiz Gustavo Ghisi Flores <ghisiluizgustavo@gmail.com>
2021-02-25 19:44:38 -0300

    Paginacao e ordenacao

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -3,16 +3,20 @@ package br.com.alura.spring.data.repository;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.spring.data.orm.Funcionario;
 
 @Repository
-public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
+public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
 
   List<Funcionario> findByNome(String nome);
+
+  List<Funcionario> findByNome(String nome, Pageable pageable);
 
   @Query("SELECT f FROM Funcionario f WHERE f.nome = :nome "
         + "AND f.salario >= :salario AND f.dataContratacao = :data")
